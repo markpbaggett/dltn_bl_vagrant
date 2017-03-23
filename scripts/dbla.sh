abe2fef60fdc58fa2124172cae49e97db0cd7639
Mark Baggett <mbagget1@utk.edu>
2017-03-23 00:01:11 -0400

    Probably break something.

--- scripts/dbla.sh
+++ scripts/dbla.sh
@@ -5,6 +5,9 @@ cd /home/vagrant || exit
 if [ ! -d /home/vagrant/dbla ]; then
   echo "Cloning DBLA"
   git clone https://github.com/barmintor/dbla
+  cd dbla
+  echo "Override old DBLA config"
+  curl -L https://gist.githubusercontent.com/markpbaggett/1d4883578a94a85292a185f150073695/raw/e722a6900f1a2d76c10b48818a60c5d35e81a4d4/dbla.gemspec > dbla.gemspec
 else
   cd dbla || exit
   echo "Updating DBLA"
