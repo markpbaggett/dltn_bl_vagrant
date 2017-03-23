91f1f2cbdb168312427e118c9d49d4bc5a1d0fc1
Mark Baggett <mbagget1@utk.edu>
2017-03-23 00:02:02 -0400

    Setup to Catalog Controller replacement.

--- scripts/setup_dbla.sh
+++ scripts/setup_dbla.sh
@@ -6,7 +6,15 @@ if [ ! -d /home/vagrant/search_app ]; then
     echo "Installing Blacklight"
     rails new search_app -m https://raw.github.com/projectblacklight/blacklight/master/template.demo.rb
     cd search_app || exit
-    echo 'gem "dbla"' >> Gemfile
+    echo "Adding dbla to Gemfile"
+    printf '\ngem "dbla", github: "barmintor/dbla", :branch => "master"' >> Gemfile
+    echo "Bundle install"
+    bundle config local.dbla /home/vagrant/dbla/
+    bundle install
+    echo "Run DBLA Generators"
+    rails g dbla:install
+    echo "Copying DPLA Credentials"
+    cp /vagrant/config/dpla.yml config/
 else
     echo "Blacklight already installed."
 fi
